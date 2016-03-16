94408be56335b44aea1dfe06ef12fde3643dfc6f
Paulog94 <paulog94@gmail.com>
2016-03-15 21:07:01 -0400

    Knight movement completed

--- chess53/src/chess53/Knight.java
+++ chess53/src/chess53/Knight.java
@@ -13,4 +13,20 @@ public class Knight extends ChessPiece {
 		return symbol;
 	}
 
+	@Override
+	public boolean isValid(int column, int row, ChessSpace[][] cb){
+
+		int R = Math.abs((super.getRow()-row));
+		int C = Math.abs((super.getColumn()-column));
+
+		if((R == 1 && C == 2) || (R == 2 && C == 1)){
+
+			if((!cb[column][row].getIsOccupied()) ||
+					(cb[column][row].getPieceOn().getColor() != super.getColor()))
+				return true;
+		}
+
+		return false;
+	}
+
 }
